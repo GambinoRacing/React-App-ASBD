0e33d69014a579cf79b28ce3869dc506c440234f
Zombayo123 <bpizhev2012@gmail.com>
2018-03-01 09:52:33 +0200

    New update

--- src/components/weather/weather.jsx
+++ src/components/weather/weather.jsx
@@ -4,8 +4,19 @@ import 'react-select/dist/react-select.css';
 import Api from '../../Api'
 import DatePicker from 'material-ui/DatePicker';
 import RaisedButton from 'material-ui/RaisedButton';
-import { Recharts, LineChart, Line } from 'recharts';
+import * as Recharts from 'recharts';
 
+const { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ComposedChart, Area, Bar } = Recharts;
+
+    const data = [
+      { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
+      { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
+      { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
+      { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
+      { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
+      { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
+      { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
+    ];
 
 export default class Weather extends Component {
   constructor(props) {
@@ -27,18 +38,6 @@ export default class Weather extends Component {
     this.onChange = this.onChange.bind(this);
     this.onSubmitHandler = this.onSubmitHandler.bind(this);
     this.handleDaysBefore = this.handleDaysBefore.bind(this);
-
-    // const {PropTypes} = React;
-    // const { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } = Recharts;
-    // const data = [
-    //   { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
-    //   { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
-    //   { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
-    //   { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
-    //   { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
-    //   { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
-    //   { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
-    // ];
   }
 
   handleDaysBefore(e) {
@@ -146,8 +145,10 @@ export default class Weather extends Component {
             </div>
           </div>
         </form>
-
-        {/* <LineChart width={600} height={300} data={this.data}
+        
+        <div className="row">
+        <div className="col-md-12">
+        <LineChart width={800} height={400} data={data}
             margin={{top: 5, right: 30, left: 20, bottom: 5}}>
        <XAxis dataKey="name"/>
        <YAxis yAxisId="left" />
@@ -157,8 +158,27 @@ export default class Weather extends Component {
        <Legend />
        <Line yAxisId="left" type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{r: 8}}/>
        <Line yAxisId="right" type="monotone" dataKey="uv" stroke="#82ca9d" />
-      </LineChart> */}
+       <Line yAxisId="right" type="monotone" dataKey="amt" stroke="#ff0f0f" />
+       <Line yAxisId="right" type="monotone" dataKey="amt" stroke="#ff0f0f" />
+      </LineChart> 
+        </div>
+        </div>
 
+        <div className="row">
+        <div className="col-md-12">   
+      <ComposedChart width={600} height={400} data={data}
+            margin={{top: 20, right: 20, bottom: 20, left: 20}}>
+          <XAxis dataKey="name"/>
+          <YAxis />
+          <Tooltip/>
+          <Legend/>
+          <CartesianGrid stroke='#f5f5f5'/>
+          <Area type='monotone' dataKey='amt' fill='#8884d8' stroke='#8884d8'/>
+          <Bar dataKey='pv' barSize={20} fill='#413ea0'/>
+          <Line type='monotone' dataKey='uv' stroke='#ff7300'/>
+       </ComposedChart>
+      </div>
+      </div>
       </div>
     );
   }
