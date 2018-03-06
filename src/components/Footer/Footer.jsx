1e0ed19d9b361f605624597165601c2caae922a7
Zombayo123 <bpizhev2012@gmail.com>
2018-03-06 15:55:05 +0200

    New update

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Card, CardFooter } from 'reactstrap';
+
+const Footer = (props) => {
+  return (
+    <div className="footer">
+      <Card>
+        <CardFooter>Footer</CardFooter>
+      </Card>
+    </div>
+  );
+};
+
+export default Example;
